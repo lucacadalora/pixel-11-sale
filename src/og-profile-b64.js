4d066df4c3a5f5e2645b32526469153f6611c37f
Luca Cada Lora <49451611+lucacadalora@users.noreply.github.com>
2026-09-14 22:45:08 +0700

    feat: wire 4-part CV OG jpeg module

--- src/og-profile-b64.js
+++ src/og-profile-b64.js
@@ -1,1 +1,5 @@
-__LOAD_FROM__/workspace/og/og-profile-b64.js
+import { P0 } from "./og-profile-b64-p0.js";
+import { P1 } from "./og-profile-b64-p1.js";
+import { P2 } from "./og-profile-b64-p2.js";
+import { P3 } from "./og-profile-b64-p3.js";
+export const OG_PROFILE_JPEG_B64 = P0 + P1 + P2 + P3;
